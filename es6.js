3091660d60006b40cc4652ed7ad03e5bef3b8576
sunidhisc <90034277+sunidhisc@users.noreply.github.com>
2023-06-06 13:47:07 +0530

    Add files via upload

---
+++ es6.js
@@ -0,0 +1,93 @@
+/*const name='Prognoz';
+console.log(name);
+const name1='Technologies';
+console.log(name1);
+
+const person={
+    name2:'Ganesh',
+    age:'21',
+    isplaced:true
+};
+console.log(person.name2);
+person.name2='Manish Das';
+console.log(person.name2);
+
+let name='Suresh';
+console.log(name);
+name='Rahul';
+console.log(name);
+
+let bike={
+    name:'Ninja',
+    engine:'998cc',
+    abs:'dual channel'
+};
+console.log(bike.engine);
+bike.engine='789cc';
+console.log(bike.engine);
+
+function printname(name)
+{
+    console.log('hello '+name)
+}
+printname('Ganesh');
+
+const printname= name=>
+{
+    return `hi ${name}`;
+}
+console.log(printname('ganesh'));
+
+const printname1=name=> `hiii ${name}`;
+console.log(printname1('manisha'));
+
+var name='Piya';
+console.log('My name is '+name);
+const name1='Vinayak';
+console.log(`My name is ${name1}`);
+
+const college={
+    name:'BPUT',
+    established:'1941',
+    ispvt:false
+};
+let name=college.name;
+let established=college.established;
+let ispvt=college.ispvt;
+console.log(name);
+console.log(established);
+console.log(ispvt);
+
+const college={
+    name:'WER',
+    established:'1998',
+    ispvt:true
+};
+let{name,established,ispvt}=college
+console.log(name);
+console.log(established);
+console.log(ispvt);
+
+const arr=['white','pink','blue'];
+let[value1,value2,value3]=arr;
+console.log(value1);
+console.log(value2);
+console.log(value3);
+
+function fun(a,b)
+{
+    return a+b;
+}
+console.log(fun(2,1));
+console.log(fun(2,1))
+console.log(fun(2));*/
+
+function fun(a,b=1)
+{
+    return a+b;
+}
+console.log(fun(2,1));
+console.log(fun(2));
+
+
+
