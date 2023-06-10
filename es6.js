302b64b30ef0f28006de63381e357ca02b455970
sunidhisc <90034277+sunidhisc@users.noreply.github.com>
2023-06-10 11:10:06 +0530

    Add files via upload

--- es6.js
+++ es6.js
@@ -80,7 +80,7 @@ function fun(a,b)
 }
 console.log(fun(2,1));
 console.log(fun(2,1))
-console.log(fun(2));*/
+console.log(fun(2));
 
 function fun(a,b=1)
 {
@@ -88,6 +88,132 @@ function fun(a,b=1)
 }
 console.log(fun(2,1));
 console.log(fun(2));
+
+function fun(a,b)
+{
+    b=(typeof b!=='undefined')?b:1;
+    return a+b;
+}
+console.log(fun(2,1));
+console.log(fun(2));
+
+class Vehicle
+{
+    constructor(name,engine)
+    {
+        this.name=name;
+        this.engine=engine;
+    }
+}
+const bike1=new Vehicle('Ninja ZX-10R','998cc');
+const bike2=new Vehicle('Duke','390cc');
+console.log(bike1.name);
+console.log(bike2.name);
+
+function fun(a,b)
+{
+    return a+b;
+}
+console.log(fun(1,2));
+console.log(fun(1,2,3,4,5));
+
+function fun(...input)
+{
+    let sum=0;
+    for (let i of input)
+    {
+        sum+=i;
+    }
+return sum;
+}
+console.log(fun(1,2));
+console.log(fun(1,2,3));
+console.log(fun(1,2,3,4,5));
+
+console.log(Math.min(1,2,3,-1));
+
+let arr=[1,2,3,-1];
+console.log(Math.min(arr));
+
+let arr=[1,2,3,-1];
+console.log(Math.min(...arr));
+
+var name="Lilly";
+var color="White";
+var age=3;
+var barkWithName=function()
+{
+    console.log('Woof Woof !!,I am '
+    +this.name+'and I am a'+this.age+
+    'years old,'+this.color+'colored dog.Woof Woof !!');
+}
+var yetanotherdog={name,color,age,barkWithName};
+yetanotherdog.barkWithName();
+
+var driver1={
+    name:"John",
+    speed:50,
+    car:"Ferrari",
+    speedUp:function(speedup)
+    {
+        this.speed=this.speed+speedup;
+        console.log("new speed="+this.speed)
+    }
+}
+
+const driver2={
+    name:"Jane",
+    speed:60,
+    car:"McLaren",
+    speedUp(speedup)
+    {
+        this.speed=this.speed+speedup;
+        console.log("new speed="+this.speed)
+    }
+}*/
+
+
+var promise=new Promise(function(resolve,reject))
+{
+    const x='Prognoztech';
+    const y='Prognoztech'
+    if(x===y)
+    {
+        resolve();
+    }
+    else
+    {
+        reject();
+    }
+    
+});
+promise.then(function()
+{
+    console.log('Success you are Prognoz');
+}).
+catch(function()
+{
+    console.log('Some error')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ m2.mjs
@@ -0,0 +1,2 @@
+import {add} from "./m1.mjs";
+console.log(add(10,20));
